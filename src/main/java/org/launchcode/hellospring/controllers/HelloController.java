119192226c11e365770de283c40c60f1e4c47f02
Cristina <cristina.gratton@gmail.com>
2022-02-03 16:14:50 -0600

    exercise work on controllers

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -21,16 +21,33 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    // Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+//    // Handles request of the form /hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
 
-    // Handles request of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+//    // Handles request of the form /hello/LaunchCode
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name){
+//        return "Hello, " + name + "!";
+//    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public static String createMessage(@RequestParam String name, @RequestParam("language") String language){
+        if (language.equalsIgnoreCase("english")) {
+            return "Hello, " + name + "!";
+        } else if (language == "french") {
+            return "Bonjour, " + name + "!";
+        } else if (language == "italian") {
+            return "Ciao, " + name + "!";
+        } else if (language == "spanish") {
+            return "Hola, " + name + "!";
+        } else if (language == "portuguese") {
+            return "Ola, " + name + "!";
+        } else {
+            return "hello";
+        }
     }
 
     // hello/form
@@ -40,6 +57,13 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" + // submit a request to /hello
                 "<input type='text' name='name'> " +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='italian'>Italian</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='portuguese'>Portuguese</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet Me!'>" +
                 "</form>" +
                 "</body>" +
